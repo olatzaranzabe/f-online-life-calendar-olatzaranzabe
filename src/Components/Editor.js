a9bc7232cf1172cd94ca0f2e252fd153f6ac7551
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-10-17 12:18:43 +0200

    create form in editor

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -3,16 +3,56 @@ import React, { Component } from 'react';
 class Editor extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <form>
-                <label>Date</label>
-                <input></input>
+                <fieldset>
+                    <legend>Date</legend>
+                    <label htmlFor="form__date"></label>
+                    <input
+                        id="form__date"
+                        className="form_date"
+                        type="text"
+                    />
+                </fieldset>
+                <fieldset>
+                    <legend>State</legend>
+                    <div>
+                        <input
+                            type="radio"
+                            id="happy"
+                            name="happy"
+                            value="happy"
+                        />
+                        <label htmlFor="happy">:)</label>
+                    </div>
+                    <div>
+                        <input
+                            type="radio"
+                            id="sad"
+                            name="sad"
+                            value="sad"
+                        />
+                        <label htmlFor="sad">:(</label>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>Message</legend>
+                    <label htmlFor="message"></label>
+                    <input
+                        id="message"
+                        className="form_date"
+                        type="text"
+                    />
+                </fieldset>
+                <div className="button">
+                    <button type="submit">Save</button>
+                </div>
             </form>
-         );
+        );
     }
 }
- 
+
 export default Editor;
